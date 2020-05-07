32399f015cac5d661783cec8363d9c750c64cfa0
Justin Lohner <lohner4@gmail.com>
2020-05-07 14:15:09 -0600

    Wrote basic test to make sure tests were working properly

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import ContactForm from './ContactForm.js';
+
+test('contact form renders', () => {
+	render(<ContactForm />);
+})
