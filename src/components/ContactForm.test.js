9b3678853766412ee17069b22059ac2938a5ced1
Justin Lohner <lohner4@gmail.com>
2020-05-07 14:31:05 -0600

    Set up tests for input fields of the form

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import ContactForm from './ContactForm.js';
 
 test('contact form renders', () => {
 	render(<ContactForm />);
+})
+
+test('user can fill out and submit form as intended', () => {
+	const { getByLabelText } = render(<ContactForm />)
+
+	fireEvent.change(getByLabelText(/first name/i), { target: { value: 'Justin' } });
+	fireEvent.change(getByLabelText(/last name/i), { target: { value: 'Lohner' } });
+	fireEvent.change(getByLabelText(/email/i), { target: { value: 'justin@justin.com' } });
+	fireEvent.change(getByLabelText(/message/i), { target: { value: 'this is a test message' } });
+
+
 })
