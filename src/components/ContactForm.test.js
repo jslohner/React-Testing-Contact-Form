056cc0086d8b219d1bd40e32c147ecc079eacd77
Justin Lohner <lohner4@gmail.com>
2020-05-07 15:43:12 -0600

    Got submit button test working and added a name attribute to the submit input

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import ContactForm from './ContactForm.js';
 
 test('contact form renders', () => {
@@ -7,12 +8,12 @@ test('contact form renders', () => {
 })
 
 test('user can fill out and submit form as intended', () => {
-	const { getByLabelText } = render(<ContactForm />)
+	const { getByRole, getByLabelText } = render(<ContactForm />)
 
 	fireEvent.change(getByLabelText(/first name/i), { target: { value: 'Justin' } });
 	fireEvent.change(getByLabelText(/last name/i), { target: { value: 'Lohner' } });
 	fireEvent.change(getByLabelText(/email/i), { target: { value: 'justin@justin.com' } });
 	fireEvent.change(getByLabelText(/message/i), { target: { value: 'this is a test message' } });
 
-
+	fireEvent.click(getByRole('button', /submit/i));
 })
